3c9231638bab0149303a0a3409fb1c04e80d755d
Ryan Riffle <ryanriffle@icloud.com>
2018-07-13 11:11:58 +0900

    Initial Commit

---
+++ Craft2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Craft2
+//
+//  Created by Ryan Riffle on 7/13/18.
+//  Copyright © 2018 Ryan Riffle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
